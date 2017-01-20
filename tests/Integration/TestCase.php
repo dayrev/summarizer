4ffe85558fe69dec49509f60e5c659ef99a58eb7
Daniel Sposito <daniel.g.sposito@gmail.com>
2017-01-20 04:24:50 -0800

    Adds phpunit dependencies and configuration.

---
+++ tests/Integration/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace DayRev\Summarizer\Tests\Integration;
+
+/**
+ * @see http://docs.mockery.io/en/latest/reference/phpunit_integration.html
+ */
+class TestCase extends \Mockery\Adapter\Phpunit\MockeryTestCase
+{
+
+}
