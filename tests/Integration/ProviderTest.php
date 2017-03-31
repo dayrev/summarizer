e3586bc5839ff68cd7388eb7c992fb3cd00f1ae3
Daniel Sposito <daniel.g.sposito@gmail.com>
2017-03-31 01:58:59 -0700

    Removes integration test mocking in favor of full API requests.

--- tests/Integration/ProviderTest.php
+++ tests/Integration/ProviderTest.php
@@ -4,9 +4,6 @@ namespace DayRev\Summarizer\Tests\Integration;
 
 use DayRev\Summarizer\Content;
 use DayRev\Summarizer\Provider;
-use Mockery;
-use ReflectionProperty;
-use stdClass;
 
 class ProviderTest extends TestCase
 {
@@ -16,44 +13,33 @@ class ProviderTest extends TestCase
         $content = $provider->summarize($this->getTextToSummarize());
 
         $this->assertInstanceOf('DayRev\Summarizer\Content', $content);
-        $this->assertEquals($this->getExpectedDayrevSummarizedContent()->text, $content->text);
+        $this->assertEquals($this->getExpectedDayrevSummary()->text, $content->text);
     }
 
     public function testSmmrySummarizesExpectedContent()
     {
-        $summarizer = Mockery::mock('DayRev\Smmry\SDK', array('api_key' => 'D74KLJ345UH9SHDF1', 'summary_length' => 4))
-            ->shouldReceive('summarizeText')
-            ->andReturn($this->getExpectedSmmrySummarizedContent())
-            ->getMock();
-
-        $provider = Provider::instance('smmry');
-        $this->setProtectedProviderProperty($provider, 'summarizer', $summarizer);
-
+        $provider = Provider::instance(
+            'smmry',
+            array('api_key' => $this->config['smmry_api_key'], 'summary_length' => 4)
+        );
         $content = $provider->summarize($this->getTextToSummarize());
 
         $this->assertInstanceOf('DayRev\Summarizer\Content', $content);
-        $this->assertEquals($this->getExpectedSmmrySummarizedContent()->sm_api_content, $content->text);
+        $this->assertEquals($this->getExpectedSmmrySummary()->text, $content->text);
     }
 
-    protected function setProtectedProviderProperty(Provider $provider, string $property, $value)
+    protected function getExpectedDayrevSummary(): Content
     {
-        $reflected_property = new ReflectionProperty(get_class($provider), $property);
-        $reflected_property->setAccessible(true);
-        $reflected_property->setValue($provider, $value);
+        $summary = new Content();
+        $summary->text = file_get_contents(__DIR__ . '/../Data/summarized-text-dayrev.txt');
+
+        return $summary;
     }
 
-    protected function getExpectedDayrevSummarizedContent(): Content
+    protected function getExpectedSmmrySummary(): Content
     {
-        $content = new Content();
-        $content->text = file_get_contents(__DIR__ . '/../Data/summarized-text-dayrev.txt');
-
-        return $content;
-    }
-
-    protected function getExpectedSmmrySummarizedContent(): stdClass
-    {
-        $summary = new stdClass();
-        $summary->sm_api_content = file_get_contents(__DIR__ . '/../Data/summarized-text-smmry.txt');
+        $summary = new Content();
+        $summary->text = file_get_contents(__DIR__ . '/../Data/summarized-text-smmry.txt');
 
         return $summary;
     }
--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -2,10 +2,7 @@
 
 namespace DayRev\Summarizer\Tests\Integration;
 
-/**
- * @see http://docs.mockery.io/en/latest/reference/phpunit_integration.html
- */
-class TestCase extends \Mockery\Adapter\Phpunit\MockeryTestCase
+class TestCase extends \PHPUnit_Framework_TestCase
 {
     protected $config = [];
 
