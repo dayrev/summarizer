3a282739f99bb1737febe75fccf77d17af4c8189
Daniel Sposito <daniel.g.sposito@gmail.com>
2017-03-31 01:57:41 -0700

    Adds support for test suite CLI argument parsing.

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -7,5 +7,32 @@ namespace DayRev\Summarizer\Tests\Integration;
  */
 class TestCase extends \Mockery\Adapter\Phpunit\MockeryTestCase
 {
+    protected $config = [];
 
+    public function __construct()
+    {
+        $this->parseParams();
+    }
+
+    protected function parseParams()
+    {
+        $params = $_SERVER['argv'];
+        foreach ($params as $index => $param) {
+            if ($param != '-d') {
+                continue;
+            }
+
+            if (!$config = $params[$index + 1] ?? false) {
+                continue;
+            }
+
+            if (strpos($config, '=') === false) {
+                continue;
+            }
+
+            list($key, $value) = explode('=', $config);
+
+            $this->config[$key] = $value;
+        }
+    }
 }
